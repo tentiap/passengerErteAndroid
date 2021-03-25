ed96df71553f3a4d2f6c2bea92e3bf1683a1259b
tentiap <inooyabu@gmail.com>
2021-03-25 10:24:02 +0700

    Makkubu setup

--- app/src/main/java/com/example/pemesanerte/api/ApiClient.java
+++ app/src/main/java/com/example/pemesanerte/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.43.23:8000/api/";
+    private static final String BASE_URL = "http://172.20.10.11:8000/api/";
 
     private static Retrofit retrofit;
 
